64f24a17ab59e605ec5869c6025b24bb4497c194
elastic <elastic@sharenj.org>
2021-04-23 06:29:45 -0700

    Add Parameter IConnection and IModel Receive

--- Send/Program.cs
+++ Send/Program.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.IO;
 using RabbitMQ.Client;
-using RabbitMQ.Util;
 using System.Text;
 
 namespace Send
@@ -15,6 +13,7 @@ namespace Send
         Password = "test",
         HostName = "master.sharenj.org",
         VirtualHost = "jkalil",
+        Port = 5671,
       };
 
       factory.Ssl.Version = System.Security.Authentication.SslProtocols.Tls12;
@@ -22,21 +21,15 @@ namespace Send
       factory.Ssl.ServerName = "master.sharenj.org";
       factory.Ssl.CertPath = @"C:\code\api\rabbitmq-client-dotnet\Certificate\server.pem";
       factory.Ssl.Enabled = true;
-      factory.Port = 5671;
 
       using(IConnection connection = factory.CreateConnection())
       using(IModel channel = connection.CreateModel())
       {
-        Console.WriteLine("Successfully connected and opened a channel");
-
         channel.QueueDeclare(queue: "TestAppQueue", durable: true, exclusive: false, autoDelete: false, arguments: null);
-        Console.WriteLine("Successfully declared a queue");
 
-        Console.WriteLine("Writing message...");
-        string message = "More Hello World More Hello World More Hello World with Ssl and consumer with ssl";
+        string message = "Hello World! 2";
         var body = Encoding.UTF8.GetBytes(message);
 
-        Console.WriteLine("Sending message...");
         channel.BasicPublish(exchange: "", routingKey: "TestAppQueue", basicProperties: null, body: body);
 
         Console.WriteLine(" [x] Sent {0}", message);
